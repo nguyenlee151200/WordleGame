748e0145513624155a2d1df98266733824e0722f
nguyenlee151200 <trongnguyen010200@gmail.com>
2022-11-14 11:13:59 +0700

    Game Word

---
+++ WordGame/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace WordGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string play = "";
+            Game wordleGame = new Game();
+            wordleGame.DisplayDetails();
+            while (true)
+            {
+                Console.WriteLine("Would you like to play My Wordle [y|n] ?");//stage 3:
+                play = Console.ReadLine();//stage 3:
+                if (play == "y" || play == "Y")
+                {
+                    wordleGame.WordleGame();
+                    //stage 11: loop checks for input characters
+                    while (true)
+                    {
+                        Console.WriteLine("Would you like to play again [y|n] ?");
+                        play = Console.ReadLine();
+                        if (play == "y" || play == "Y")
+                        {
+                            wordleGame.WordleGame();
+                        }
+                        else if (play == "n" || play == "N")
+                        {
+                            Console.WriteLine("Thank you !!!");
+                            return;
+                        }
+                    }
+                }
+                else if (play == "n" || play == "N")
+                {
+                    Console.WriteLine("Thank you !!!");
+                    return;
+                }
+            }
+
+        }
+    }
+    
+}
